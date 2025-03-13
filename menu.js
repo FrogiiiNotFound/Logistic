b93eba9f00ab7c1157dfa0db8244534abdf345ed
FrogiiiNotFound <119341470+FrogiiiNotFound@users.noreply.github.com>
2025-03-13 16:45:21 +0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,9 +1,13 @@
 const body = document.querySelector('body');
 const menuIcon = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
+const bodyItems = document.querySelectorAll(".nav-header__link")
 
 menuIcon.addEventListener('click', (e) => {
     menuIcon.classList.toggle('_active');
     menuBody.classList.toggle('_active');
     body.classList.toggle('_active');
+    bodyItems.forEach((e) => {
+        e.classList.add("link-1")
+    })
 })
